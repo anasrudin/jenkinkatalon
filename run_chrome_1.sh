6b4f68c2ef64f3b959957a59d748f9a210780ab2
anasrudin <a.nasrudin999@gmail.com>
2022-09-08 15:04:36 +0700

    add chmod

--- run_chrome_1.sh
+++ run_chrome_1.sh
@@ -1,1 +1,2 @@
- sudo /home/ubuntu/Katalon_Studio_Engine_Linux_64-8.4.1/katalonc -retry=0 -testSuitePath="Test Suites/TS_RegressionTest" -browserType="Chrome (headless)" -executionProfile="default" --config -proxy.auth.option=NO_PROXY -proxy.system.option=NO_PROXY -proxy.system.applyToDesiredCapabilities=true
+sh "chmod +x -R ${env.WORKSPACE}"
+sh /home/ubuntu/Katalon_Studio_Engine_Linux_64-8.4.1/katalonc -retry=0 -testSuitePath="Test Suites/TS_RegressionTest" -browserType="Chrome (headless)" -executionProfile="default" --config -proxy.auth.option=NO_PROXY -proxy.system.option=NO_PROXY -proxy.system.applyToDesiredCapabilities=true

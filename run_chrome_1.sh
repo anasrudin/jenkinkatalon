2136f2608c14520df3b081c93d6dc8df5461cae4
anasrudin <a.nasrudin999@gmail.com>
2022-09-08 15:06:53 +0700

    add env

--- run_chrome_1.sh
+++ run_chrome_1.sh
@@ -1,2 +1,6 @@
-sh "chmod +x -R ${env.WORKSPACE}"
-sh /home/ubuntu/Katalon_Studio_Engine_Linux_64-8.4.1/katalonc -retry=0 -testSuitePath="Test Suites/TS_RegressionTest" -browserType="Chrome (headless)" -executionProfile="default" --config -proxy.auth.option=NO_PROXY -proxy.system.option=NO_PROXY -proxy.system.applyToDesiredCapabilities=true
+#!/usr/bin/env bash
+
+set -xe
+
+
+/home/ubuntu/Katalon_Studio_Engine_Linux_64-8.4.1/katalonc -retry=0 -testSuitePath="Test Suites/TS_RegressionTest" -browserType="Chrome (headless)" -executionProfile="default" --config -proxy.auth.option=NO_PROXY -proxy.system.option=NO_PROXY -proxy.system.applyToDesiredCapabilities=true
